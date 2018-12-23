638349edb7c971eabeab472abed1da3f1d6897d3
gozdur.sebastian <gozdur.sebastian@gmail.com>
2018-12-23 22:57:15 +0100

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var v1 = require('./routes/v1');
+
+var app = express();
+var port = process.env.PORT || '3000';
+
+var mysql = require("mysql");
+//Database connection
+app.use(function(req, res, next){
+    global.connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'password',
+        database : 'webdevproject'
+    });
+    connection.connect();
+    next();
+});
+app.use('/api/v1', v1);
+
+
+app.listen(port);
+console.log('Server started on: '+ port );
+
+module.exports = app;
